77a69bb156a68fb5683036f8b7dbb6e6d80069ce
Haresh Vekriya <h.vekriya@kainos.com>
2017-11-30 18:13:16 +0000

    email error

--- js/fav.js
+++ js/fav.js
@@ -33,7 +33,6 @@ if (localStorage.length == 0) {
   }
 }
 
-
 function getMailBody () {
   var body = ''
   if (localStorage.length == 0) {
@@ -42,44 +41,49 @@ function getMailBody () {
     for (var i = 0; i < localStorage.length; i++) {
       if (localStorage.key(i).includes('news')) {
         body +=
-        '<h1>News</h1>' +
-        '<ul data-role="listview" data-inset="true"> ' +
-        '<li>'+ localStorage.getItem(localStorage.key(i)) + '</li>' +
-        '</ul>'
+          '<h1>News</h1>' +
+          '<ul data-role="listview" data-inset="true"> ' +
+          '<li>' + localStorage.getItem(localStorage.key(i)) + '</li>' +
+          '</ul>'
       } else if (localStorage.key(i).includes('about')) {
         body +=
-        '<h1>About</h1>' +
-        '<ul data-role="listview" data-inset="true"> ' +
-        '<li>'+ localStorage.getItem(localStorage.key(i)) + '</li>' +
-        '</ul>'
+          '<h1>About</h1>' +
+          '<ul data-role="listview" data-inset="true"> ' +
+          '<li>' + localStorage.getItem(localStorage.key(i)) + '</li>' +
+          '</ul>'
       } else if (localStorage.key(i).includes('events')) {
         body +=
-        '<h1>Events</h1>' +
-        '<ul data-role="listview" data-inset="true"> ' +
-        '<li>'+ localStorage.getItem(localStorage.key(i)) + '</li>' +
-        '</ul>'
+          '<h1>Events</h1>' +
+          '<ul data-role="listview" data-inset="true"> ' +
+          '<li>' + localStorage.getItem(localStorage.key(i)) + '</li>' +
+          '</ul>'
       } else if (localStorage.key(i).includes('stores')) {
         body +=
-        '<h1>Stores</h1>' +
-        '<ul data-role="listview" data-inset="true"> ' +
-        '<li>'+ localStorage.getItem(localStorage.key(i)) + '</li>' +
-        '</ul>'
+          '<h1>Stores</h1>' +
+          '<ul data-role="listview" data-inset="true"> ' +
+          '<li>' + localStorage.getItem(localStorage.key(i)) + '</li>' +
+          '</ul>'
       }
     }
   }
-  body +='<br><br><p>Thanks,<br>BLURB book stores</p>'
+  body += '<br><br><p>Thanks,<br>BLURB book stores</p>'
   return body
 }
 
 $('.sendEmail').click(function (event) {
-  Email.send('w1497109@my.westminster.ac.uk',
-    $('#email').val(),
-    'Favourites List',
-    getMailBody(),
-    {token: 'bc296314-2062-48ef-ade3-e9cc38cad316',
-      callback: function done (message) {
-        $('.email').html('Email sent to: ' + $('#email').val())
-        $('#email-sent').popup('open')
-      }
-    })
+  if ($('#email').val() == '') {
+    $('.error-message').html('Email field can not be left blank.').show()
+  } else {
+    Email.send('w1497109@my.westminster.ac.uk',
+      $('#email').val(),
+      'Favourites List',
+      getMailBody(),
+      {token: 'bc296314-2062-48ef-ade3-e9cc38cad316',
+        callback: function done (message) {
+          $('.email').html('Email sent to: ' + $('#email').val())
+          $('#email-sent').popup('open')
+        }
+      })
+   $('.error-message').hide()
+  }
 })
