35afa3323d1619beef945b13d9794fe25636e1f2
Haresh Vekriya <h.vekriya@kainos.com>
2017-11-30 16:13:07 +0000

    sending html in email body

--- js/fav.js
+++ js/fav.js
@@ -34,47 +34,49 @@ if (localStorage.length == 0) {
 }
 
 function getMailBody () {
-
   var body = ''
-
-  for (var i = 0; i < localStorage.length; i++) {
-    if (localStorage.key(i).includes('news')) {
-        body =+
-        '<h1>News</h1>'
+  if (localStorage.length == 0) {
+    body = 'List is empty'
+  } else {
+    for (var i = 0; i < localStorage.length; i++) {
+      if (localStorage.key(i).includes('news')) {
+        body +=
+          '<h1>News</h1>'
         '<ul data-role="listview" data-inset="true"> ' +
         '<li><a href="' + localStorage.key(i) + '" rel="external">' + localStorage.getItem(localStorage.key(i)) + '</a></li>' +
         '</ul>'
-    } else if (localStorage.key(i).includes('about')) {
-        body =+
-        '<h1>About</h1>'
+      } else if (localStorage.key(i).includes('about')) {
+        body +=
+          '<h1>About</h1>'
         '<ul data-role="listview" data-inset="true"> ' +
         '<li><a href="' + localStorage.key(i) + '" rel="external">' + localStorage.getItem(localStorage.key(i)) + '</a></li>' +
         '</ul>'
-    } else if (localStorage.key(i).includes('events')) {
-        body =+
-        '<h1>Events</h1>'
+      } else if (localStorage.key(i).includes('events')) {
+        body +=
+          '<h1>Events</h1>'
         '<ul data-role="listview" data-inset="true"> ' +
         '<li><a href="' + localStorage.key(i) + '" rel="external">' + localStorage.getItem(localStorage.key(i)) + '</a></li>' +
         '</ul>'
-    } else if (localStorage.key(i).includes('stores')) {
-        body =+
-        '<h1>Stores</h1>'
+      } else if (localStorage.key(i).includes('stores')) {
+        body +=
+          '<h1>Stores</h1>'
         '<ul data-role="listview" data-inset="true"> ' +
         '<li><a href="' + localStorage.key(i) + '" rel="external">' + localStorage.getItem(localStorage.key(i)) + '</a></li>' +
         '</ul>'
+      }
     }
+    return body
   }
-  return body
 }
 
 $('.sendEmail').click(function (event) {
   Email.send('w1497109@my.westminster.ac.uk',
     $('#email').val(),
     'Favourites List',
-    getMailBody(),
+    getMailBody() + $("html").html(),
     {token: 'bc296314-2062-48ef-ade3-e9cc38cad316'},
     function done (message) {
-        $('.email').html('Email sent to:' + $('#email').val())
-        $( "#emai-sent" ).popup( "open" );
+      $('.email').html('Email sent to:' + $('#email').val())
+      $('#email-sent').popup('open')
     })
 })
