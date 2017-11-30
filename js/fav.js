5ff94087a085174f7ec53527ba293eea5ebacfed
Haresh Vekriya <h.vekriya@kainos.com>
2017-11-30 17:34:57 +0000

    sending the whole fav div

--- js/fav.js
+++ js/fav.js
@@ -33,47 +33,11 @@ if (localStorage.length == 0) {
   }
 }
 
-function getMailBody () {
-  var body = ''
-  if (localStorage.length == 0) {
-    body = 'List is empty'
-  } else {
-    for (var i = 0; i < localStorage.length; i++) {
-      if (localStorage.key(i).includes('news')) {
-        body +=
-        '<h1>News</h1>' +
-        '<ul data-role="listview" data-inset="true"> ' +
-        '<li><a href="' + localStorage.key(i) + '" rel="external">' + localStorage.getItem(localStorage.key(i)) + '</a></li>' +
-        '</ul>'
-      } else if (localStorage.key(i).includes('about')) {
-        body +=
-        '<h1>About</h1>' +
-        '<ul data-role="listview" data-inset="true"> ' +
-        '<li><a href="' + localStorage.key(i) + '" rel="external">' + localStorage.getItem(localStorage.key(i)) + '</a></li>' +
-        '</ul>'
-      } else if (localStorage.key(i).includes('events')) {
-        body +=
-        '<h1>Events</h1>' +
-        '<ul data-role="listview" data-inset="true"> ' +
-        '<li><a href="' + localStorage.key(i) + '" rel="external">' + localStorage.getItem(localStorage.key(i)) + '</a></li>' +
-        '</ul>'
-      } else if (localStorage.key(i).includes('stores')) {
-        body +=
-        '<h1>Stores</h1>' +
-        '<ul data-role="listview" data-inset="true"> ' +
-        '<li><a href="' + localStorage.key(i) + '" rel="external">' + localStorage.getItem(localStorage.key(i)) + '</a></li>' +
-        '</ul>'
-      }
-    }
-  }
-  return body
-}
-
 $('.sendEmail').click(function (event) {
   Email.send('blurb@info.com',
     $('#email').val(),
     'Favourites List',
-    getMailBody(),
+    $('.fav').html(),
     {token: 'bc296314-2062-48ef-ade3-e9cc38cad316',
       callback: function done (message) {
         $('.email').html('Email sent to:' + $('#email').val())
